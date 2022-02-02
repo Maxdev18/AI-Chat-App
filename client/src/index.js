cc48e238893b3085cfb1a8585b897803edb00c15
Maxzm6958 <maxpersonal1721@gmail.com>
2022-02-02 15:42:13 -0500

    Added boiler plate and established a websocket connection with the server

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,20 @@
 import ReactDOM from 'react-dom';
+import * as React from 'react';
+import './styles/home.css';
+import io from "socket.io-client";
+
+const socket = io('http://localhost:4000');
 
 function App() {
+  let [text, setText] = React.useState('');
+
+  socket.on('message', (data) => {
+    setText(data);
+  });
+
   return (
     <div className="App">
-      Home
+      {text}
     </div>
   );
 }
