366c0f46bf4c7dc93513ce9e57dd3c33eb2d5e42
Maxzm6958 <maxpersonal1721@gmail.com>
2022-03-09 16:29:08 -0500

    Template for blog page

---
+++ client/src/pages/howWasItMade.js
@@ -0,0 +1,22 @@
+import { React, Navigation, Footer } from '../client-imports';
+import '../styles/pages/howWasItMade.css';
+
+export const HowWasItMade = () => {
+  return (
+    <div className="howMade-main-container">
+      <Navigation />
+      <div className="howMade-container">
+        {/* <h1 className="howMade-title">How was it made?</h1>
+
+        <div className="howMade-desc-container">
+          <h2 className="howMade-subtitle">Phase 1: The Design</h2>
+          <p className="howMade-desc">
+
+          </p>
+        </div> */}
+        <h1 className='temp-h1'>This blog post will be up when the application is released officially on version 1.0.0, stay tuned!</h1>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  )
+};
