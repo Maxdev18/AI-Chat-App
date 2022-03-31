e3028fc4a7677fa0492e7c4c5e22713050b93244
Maxzm6958 <maxpersonal1721@gmail.com>
2022-03-30 23:27:48 -0400

    Maybe now?

--- client/src/client-components/footer.js
+++ client/src/client-components/footer.js
@@ -1,4 +1,5 @@
-import { React, Link } from '../client-imports';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/footer.css';
 
 export const Footer = () => {
