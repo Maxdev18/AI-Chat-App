a4cf9af3163847205d7cb7d1b9d7008df666d2b5
Maxzm6958 <maxpersonal1721@gmail.com>
2022-05-16 15:15:13 -0400

    added some application toggle state

--- client/src/contexts/contexts.js
+++ client/src/contexts/contexts.js
@@ -3,3 +3,7 @@ import { createContext } from 'react';
 export const UserContext = createContext(null);
 
 export const UserLoggedIn = createContext(false);
+
+export const SettingToggle = createContext(false);
+
+export const CreateRoomToggle = createContext(false);
