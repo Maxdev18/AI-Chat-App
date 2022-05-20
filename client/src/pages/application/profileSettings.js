f2ce21bca942d12494f3a05cffefeb5cb3e46638
Maxzm6958 <maxpersonal1721@gmail.com>
2022-05-20 09:54:25 -0400

    Made part of upload image functionality for profile

--- client/src/pages/application/profileSettings.js
+++ client/src/pages/application/profileSettings.js
@@ -4,20 +4,46 @@ import '../../styles/pages/application/profileSettings.css';
 
 export const ProfileSettings = () => {
   const { user, setuser } = React.useContext(UserContext);
-  let [ bio, setBio ] = React.useState('');
-  let bool;
+  let [ name, setName ] = React.useState(user.name);
+  let [ bio, setBio ] = React.useState(user.settings.bio);
+  let [ image, setImage ] = React.useState(user.settings.profilePic.pic);
+  const reader = new FileReader();
 
-  React.useEffect(() => {
-    
-  });
-
-  function updateProfile() {
-    // axios post here
+  let profileData = {
+    id: user._id,
+    name,
+    bio
   }
 
-  function updateProfilePic() {
-    const profile = document.getElementById('');
+  function updateProfile() {
+    Axios.post('/api/application/update/update-profile', profileData)
+      .catch(err=> {
+        if(err) console.error(err);
+      });
+  }
 
+  function updateProfilePic(e) {
+    e.preventDefault();
+    const file = e.target.files[0];
+    // this is not needed in this case but still a good habit
+    reader.onloadend = () => {
+      setImage(reader.result);
+      console.log(file);
+    }
+    reader.readAsDataURL(file);
+
+    Axios.post('/api/application/update/update-profile', file)
+      .then(data => {
+        const f = data.data.file;
+        reader.onloadend = () => {
+          setImage(reader.result)
+          console.log(image);
+        }
+        reader.readAsDataURL(f);
+      })
+      .catch(err => {
+        if(err) console.error(err);
+      });
   }
 
   const renderProfile = () => {
@@ -27,13 +53,23 @@ export const ProfileSettings = () => {
 
     if(user.googleSignIn) {
       return (
-        <img src={user.settings.profilePic.pic} className="user-profile-setting" alt="profile image"></img>
+        <img src={image} className="user-profile-setting" alt="profile image"></img>
       )
     } else {
       return (
-        <div className="user-profile-setting" style={profileStyles} onClick={updateProfilePic}>
-          {user.settings.profilePic.pic}
-        </div>
+        <label htmlFor="inputTag" className="user-profile-setting" onChange={updateProfilePic}>
+          {image.length > 1 ? (
+            <>
+              <img className="profileImg" src={image} alt="profile image"/>
+              <input id="inputTag" type="file" accept="image/png, image/jpg, image/gif, image/jpeg"/>
+            </>
+          ) : (
+            <div className="noProfileImg" style={profileStyles}>
+              {user.settings.profilePic.pic}
+              <input id="inputTag" type="file" accept="image/png, image/jpg, image/gif, image/jpeg"/>
+            </div>
+          )}
+        </label>
       )
     }
   }
@@ -42,10 +78,23 @@ export const ProfileSettings = () => {
     <div className="profile-settings-container">
       <div className="profile-cont">
         {user ? renderProfile() : null}
-        <textarea className="bio" placeholder="Your bio..." maxLength="300" value={bio} onChange={e => setBio(e.target.value)} />
+        <div className="input-profile-container">
+          <span>Biography</span>
+          <textarea className="bio" placeholder="Your bio..." maxLength="300" value={bio} onChange={e => setBio(e.target.value)} />
+        </div>
       </div>
       <div className="profile-desc">
-
+        <div className="input-profile-container input-profile-main-cont">
+          <span>Name</span>
+          <input value={name} placeholder="Name..." onChange={e => setName(e.target.value)}/>
+        </div>
+        <div className="input-profile-main-cont">
+          <span>Your ID: </span>
+          {user.userAppId}
+        </div>
+      </div>
+      <div className="save-btn-cont">
+        <button className="save-btn" onClick={updateProfile}>Save</button>
       </div>
     </div>
   )
