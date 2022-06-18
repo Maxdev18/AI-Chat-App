0346726c60a81ccfce178364db89d7bd20f4738d
Maxzm6958 <maxpersonal1721@gmail.com>
2022-06-18 16:31:07 -0400

    added profile update for messages and fixed uploading images bug

--- server/controllers/update.controller.js
+++ server/controllers/update.controller.js
@@ -1,45 +1,6 @@
 const User = require('../models/userSchema');
 const path = require('path');
 
-exports.updateProfile = async (req, res) => {
-  const newProfile = req.body;
-
-  if(newProfile.name) {
-    await User.findOneAndUpdate({ _id: newProfile.id }, { 
-      name: newProfile.name,
-      'settings.bio': newProfile.bio
-    }).then(() => {
-      res.status(200).json({ message: "Update successful..."});
-    }).catch(err=> {
-      res.status(500).json({message: "Update failed..."});
-      console.error(err);
-    });
-  } else if(req.file) {
-    // save uploaded images to the database
-    if(req.rawHeaders[1] === 'localhost:5000') {
-      await User.findOneAndUpdate({ _id: newProfile.id }, {
-        'settings.profilePic.pic': 'http://localhost:5000/images/' + req.file.originalname,
-        'settings.profilePic.picName': req.file.originalname
-      }).then(() => {
-        res.status(200).json({ imgUrl: 'http://localhost:5000/images/' + req.file.originalname, message: "Update successful..."});
-      }).catch(err=> {
-        res.status(500).json({message: "Update failed..."});
-        console.error(err);
-      }) 
-    } else {
-      await User.findOneAndUpdate({ _id: newProfile.id }, {
-        'settings.profilePic.pic': 'https://chattingai-backend.herokuapp.com/images/' + req.file.originalname,
-        'settings.profilePic.picName': req.file.originalname
-      }).then(() => {
-        res.status(200).json({ imgUrl: 'https://chattingai-backend.herokuapp.com/images/' + req.file.originalname, message: "Update successful..."});
-      }).catch(err=> {
-        res.status(500).json({message: "Update failed..."});
-        console.error(err);
-      }) 
-    }
-  }
-}
-
 exports.getProfile = async (req, res) => {
   const imgUrl = req.params.imgUrl;
 
--- server/routes/update.router.js
+++ server/routes/update.router.js
@@ -1,26 +1,70 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const User = require('../models/userSchema');
+const Message = require('../models/messageSchema');
+
+let newId;
+let newFileName;
+const generateNewId = () => {
+  newId = uuidv4();
+  return newId;
+}
 
 // save uploaded images to the server
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    if(file.originalname !== `${path.join(__dirname, '..', '..', '/images') + '/' + file.originalname}`) {
-      cb(null, './images');
-    } else {
-      return null
-    }
+    cb(null, './images');
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const fileName = file.originalname.split('.');
+    newFileName = fileName[0] + "-" + generateNewId() + "." + fileName[1]
+    cb(null, newFileName);
   }
 });
 
 const upload = multer({storage: fileStorageEngine});
 
-const updateControllers = require('../controllers/update.controller');
+router.post('/update-profile', upload.single('file'), async (req, res) => {
+  const newProfile = req.body;
 
-router.post('/update-profile', upload.single('file'), updateControllers.updateProfile);
+  if(newProfile.name) {
+    await User.findOneAndUpdate({ _id: newProfile.id }, { 
+      name: newProfile.name,
+      'settings.bio': newProfile.bio
+    }).then(() => {
+      res.status(200).json({ message: "Update successful..."});
+    }).catch(err=> {
+      res.status(500).json({message: "Update failed..."});
+      console.error(err);
+    });
+  } else if(req.file) {
+    // save uploaded images to the database
+    if(req.rawHeaders[1] === 'localhost:5000') {
+      await User.findOneAndUpdate({ _id: newProfile.id }, {
+        'settings.profilePic.pic': 'http://localhost:5000/images/' + newFileName,
+        'settings.profilePic.picName': newFileName
+      }).then(async (user) => {
+        await Message.updateMany({sender: user._id}, {'senderProfile.picUrl': 'http://localhost:5000/images/' + newFileName});
+        res.status(200).json({ imgUrl: 'http://localhost:5000/images/' + newFileName, message: "Update successful..."});
+      }).catch(err=> {
+        res.status(500).json({message: "Update failed..."});
+        console.error(err);
+      }) 
+    } else {
+      await User.findOneAndUpdate({ _id: newProfile.id }, {
+        'settings.profilePic.pic': 'https://chattingai-backend.herokuapp.com/images/' + newFileName,
+        'settings.profilePic.picName': newFileName
+      }).then(async (user) => {
+        await Message.updateMany({sender: user._id}, {'senderProfile.picUrl': 'https://chattingai-backend.herokuapp.com/images/' + newFileName});
+        res.status(200).json({ imgUrl: 'https://chattingai-backend.herokuapp.com/images/' + newFileName, message: "Update successful..."});
+      }).catch(err=> {
+        res.status(500).json({message: "Update failed..."});
+        console.error(err);
+      }) 
+    }
+  }
+});
 
 module.exports = router;
